7f47d4ee4f33cf5ebc743d6e5e8e409b56d4deb0
Alexander Plaikner <alexander.plaikner@tum.de>
2024-04-28 09:44:10 +0200

    Update process vma growth test

--- benchmarks/stack_vma_tests/test4.c
+++ benchmarks/stack_vma_tests/test4.c
@@ -12,6 +12,11 @@ int main() {
 	char jeff[what*1024-sizeof(what)];
 	for(int i = 0; i < what*1024-sizeof(what); i++) {
 		jeff[i] = 'a';
+	}	scanf("%d", &what);
+
+	char jeff2[what*1024];
+	for(int i = 0; i < what*1024; i++) {
+		jeff2[i] = 'b';
 	}
 	sleep(5000);
 }
